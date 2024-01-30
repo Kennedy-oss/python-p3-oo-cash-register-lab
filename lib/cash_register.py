e3af7598e5d223c80f65eae41f443b2fd086a73b
Kennedy-oss <kennedy.onyango4@gmail.com>
2024-01-30 21:17:59 +0300

    Completed assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction = None
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title] * quantity)
+        self.last_transaction = (title, price, quantity)
+
+    def apply_discount(self):
+        if self.discount:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            # Formatting the total as an integer if it is a whole number
+            if self.total.is_integer():
+                formatted_total = int(self.total)
+            else:
+                formatted_total = f"{self.total:.2f}"
+            print(f"After the discount, the total comes to ${formatted_total}.")
+        else:
+            print("There is no discount to apply.")
+
+    
+    def void_last_transaction(self):
+        if self.last_transaction:
+            title, price, quantity = self.last_transaction
+            self.total -= price * quantity
+            for _ in range(quantity):
+                self.items.remove(title)
+        self.last_transaction = None
+
+
+
+
